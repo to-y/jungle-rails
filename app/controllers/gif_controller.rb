a1ef1e623c2ef0b1ae87346cc79608fdf77f0572
to-y <yitong922@gmail.com>
2016-10-14 03:23:06 +0000

    register button, login, logout

---
+++ app/controllers/gif_controller.rb
@@ -0,0 +1,12 @@
+
+class GifController < ApplicationController
+
+  before_filter :authorize
+
+  def cool
+  end
+
+  def free
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  resources :users
 
   resource :cart, only: [:show] do
     put    :add_item
@@ -17,6 +18,23 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:update, :edit, :show]
   end
+
+    # This route sends requests to our naked url to the *cool* action in the *gif* controller.
+
+    # I've created a gif controller so I have a page I can secure later.
+    # This is optional (as is the root to: above).
+    get '/cool' => 'gif#cool'
+    get '/sweet' => 'gif#sweet'
+    # these routes are for showing users a login form, logging them in, and logging them out.
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    get '/logout' => 'sessions#destroy'
+
+    # These routes will be for signup. The first renders a form in the browse, the second will
+    # receive the form and create a user in our database using the data given to us by the user.
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
